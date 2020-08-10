6900aef6933c69083c624c3eff9b98c9881e83f4
Eric <ttttt123465@gmail.com>
2020-08-10 22:22:25 +0800

    destruct current state when dispatch

--- src/reducer/counterReducer.js
+++ src/reducer/counterReducer.js
@@ -1,9 +1,15 @@
 export const counterReducer = (state, action) => {
     switch (action.type) {
         case 'increment':
-            return {count: state.count + 1};
+            return {
+                ...state,
+                count: state.count + 1
+            };
         case 'decrement':
-            return {count: state.count - 1};
+            return {
+                ...state,
+                count: state.count - 1
+            };
         default:
             return {
                 ...state
